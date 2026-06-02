8db6857d7ac5ab88f1fdee0393be494494a649c1
sushihead8901 <r00014250@gmail.com>
2026-06-02 09:12:22 -0700

    Add files via upload

---
+++ action.js
@@ -0,0 +1,10 @@
+/* +Addtion -Subtraction *Mutilplacation /Division */
+let username;
+document.getElementById("myButton").onclick = function () {
+    username =  document.getElementById("username") .value;
+    document.getElementById("usernameH4").textContent = `Welcome ${username}`
+}
+document.getElementById("myButthon").onclick = function(event) {
+      event.preventDefault();
+      alert("HAHAHAAHAHAHA, you thought you made an account??!? welp, i deleted it! :) there was never rewards, points, and DISCOUNTS!?!?!  you wasted your time on making a account :O. fuck off bitch, this is AMERICA 🖕:)");
+    };
